a33406009fcab0ff2f01376643aaeff0e214116c
chandu0101 <chandu.csu2010@gmail.com>
2015-03-24 06:53:21 +0530

    push form CI

--- scalajs-land/offline.js
+++ scalajs-land/offline.js
@@ -14,15 +14,13 @@
 
     //version
     
-    var version = "0.0.1"
+    var version = "0.0.2"
 
     // Cache name definitions
     var cacheNameStatic = "scalajs-land-"+version;
-    var cacheNameGithub = "scalajs-land-github";
-        
+
     var currentCacheNames = [
-        cacheNameStatic,
-        cacheNameGithub
+        cacheNameStatic
     ];
 
 
@@ -61,61 +59,34 @@
     // when a network request made from app
     self.addEventListener("fetch", function (event) {
         var requestURL = new URL(event.request.url);
-        if (requestURL.hostname.indexOf(".github.com") > -1) {
-           event.respondWith(githubAPIResponse(event.request))
-        } else {
-            event.respondWith(
-                caches.match(event.request)
-                    .then(function (response) {
-                        if (response) {  // if we cached already just send that response back
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {  // if we cached already just send that response back
+                        return response;
+                    }
+                    var fetchRequest = event.request.clone();
+                    return fetch(fetchRequest).then( // if not cached get from network return it and then cache it
+                        function (response) {
+                            var shouldCache = false;
+                            if (response.type === "basic" && response.status === 200) {
+                                shouldCache = cacheNameStatic;
+                            }
+                            if (shouldCache) {
+                                var responseToCache = response.clone();
+                                caches.open(shouldCache)
+                                    .then(function (cache) {
+                                        var cacheRequest = event.request.clone();
+                                        cache.put(cacheRequest, responseToCache);
+                                    });
+                            }
                             return response;
                         }
-                        var fetchRequest = event.request.clone();
-                        return fetch(fetchRequest).then( // if not cached get from network return it and then cache it
-                            function (response) {
-                                var shouldCache = false;
-                                if (response.type === "basic" && response.status === 200) {
-                                    shouldCache = cacheNameStatic;
-                                }
-                                if (shouldCache) {
-                                    var responseToCache = response.clone();
-                                    caches.open(shouldCache)
-                                        .then(function (cache) {
-                                            var cacheRequest = event.request.clone();
-                                            cache.put(cacheRequest, responseToCache);
-                                        });
-                                }
-                                return response;
-                            }
-                        );
-                    })
-            );
-        }
+                    );
+                })
+        );
     });
 
-    /**
-     *  get scalajs github repos.
-      * @param request
-     * @returns {*}
-     */
-  function githubAPIResponse(request) {
-      if (request.headers.get('Accept') == 'x-cache/only') {
-          return caches.match(request).then(function(response) {
-              return response;
-          });
-      } else {
-          return fetch(request.clone()).then(function(response) {
-              if(response.status === 200 ) { // only cache successful responses , as github supports CORS we can check status of response for non supported cors origins we'll get opaque response
-                  return caches.open(cacheNameGithub).then(function(cache) {
-                      cache.put(request,response.clone())
-                      return response;
-                  }); 
-              } else {
-                  return response;
-              }
-              
-          });
-      }
-  }  
+
 
 })();
